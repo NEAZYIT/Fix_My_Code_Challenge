2cccfe190bec6f7217fab3399fe5078b96d9274a
NEAZYIT <touihnazih28@gmail.com>
2023-10-20 05:54:37 -0700

    Refactor user authentication code to properly hash and validate passwords with MD5 hashing.

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -97,7 +97,7 @@ password")
         print("is_valid_password should return False if compared with None")
 
     if user_1.is_valid_password(89):
-        print("is_valid_password should return False if compared with an integer")
+        print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
